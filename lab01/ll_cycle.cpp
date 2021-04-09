554255bda35be61d6f956353801ce7220a163083
Kohan Danila 953503 <ew1011735@gmail.com>
2021-04-09 11:57:15 +0300

    Made ex.6

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,6 +1,46 @@
 #include "ll_cycle.h"
 
 bool ll_has_cycle(node *head) {
+    if(head == nullptr)
+    {
+        return 0;
+    }
+    node *hare, *snail;
+    hare = head;
+    snail = head;
+    int value = 2;
+    do
+    {
+        if(hare->next != nullptr)
+        {
+            hare = hare->next;
+            snail = snail->next;
+            if(hare->next != nullptr)
+            {
+                hare = hare->next;
+                if(hare == snail)
+                {
+                    value = 1;
+                }
+            }
+            else
+            {
+                value = 0;
+            }
+        }
+        else
+        {
+            value = 0;
+        }
+    }while(value == 2);
+    if(value == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
     /* Ваш код должен быть написан только внутри этой функции */
     return true;
 }
