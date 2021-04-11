1840ab52f63a23f38d401108712b16f35addf0f7
Kohan Danila 953503 <ew1011735@gmail.com>
2021-04-11 23:19:39 +0300

    Solved Bit ops skipping make

--- lab01/bit_ops.cpp
+++ lab01/bit_ops.cpp
@@ -4,21 +4,26 @@
 // Предполагается 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
+    x = x >> n;
     // YOUR CODE HERE
     // Взврат -1 бессмысленный и сделан для правильной компиляции
-    return -1;
+    return x & 1;
 }
 // Выставляет значение v в бите номер n
 // Предполагается что 0 <= n <= 31 и v равно 0 или 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
+    unsigned isolator = ~(1 << n);
+    v = v << n;
+    *x = (*x & isolator) | v;
     // YOUR CODE HERE
 }
 // Меняет значение бита номер n на противоположное.
 // Предполагается что 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
+    *x = (1 << n) ^ *x;
     // YOUR CODE HERE
 }
 
