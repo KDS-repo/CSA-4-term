b7318590ee09df66bb78f5c0c467769740080af9
Kohan Danila 953503 <ew1011735@gmail.com>
2021-04-12 10:24:00 +0300

    Solved Make.txt Something wrong in 8

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,13 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
+    uint16_t bit = 1 & *reg;
+    bit = (1 & (*reg >> 2)) ^ bit;
+    bit = (1 & (*reg >> 3)) ^ bit;
+    bit = (1 & (*reg >> 5)) ^ bit;
+    bit = bit << 16;
+    *reg = *reg >> 1;
+    *reg = *reg | bit;
     /* YOUR CODE HERE */
 }
 
