131ef49fd4f64b9f01b865f2b755380fe64418e6
Kohan Danila 953503 <ew1011735@gmail.com>
2021-04-12 10:26:14 +0300

    Solved 8

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -6,7 +6,7 @@ void lfsr_calculate(uint16_t *reg) {
     bit = (1 & (*reg >> 2)) ^ bit;
     bit = (1 & (*reg >> 3)) ^ bit;
     bit = (1 & (*reg >> 5)) ^ bit;
-    bit = bit << 16;
+    bit = bit << 15;
     *reg = *reg >> 1;
     *reg = *reg | bit;
     /* YOUR CODE HERE */
