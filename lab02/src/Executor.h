14b6865727a8aacdd1be8a5523544649115700db
Kohan Danila 953503 <ew1011735@gmail.com>
2021-05-14 21:30:13 +0300

    Solved lab01. Started Executor lab02

--- lab02/src/Executor.h
+++ lab02/src/Executor.h
@@ -10,6 +10,21 @@ public:
     void Execute(InstructionPtr& instr, Word ip)
     {
         /* YOUR CODE HERE */
+        switch(instr._aluFunc)
+        {
+            case AluFunc::Add: instr._data = _src1
+            case AluFunc::Sub: instr._data = 
+            case AluFunc::And: instr._data = 
+            case AluFunc::Or: instr._data = 
+            case AluFunc::Xor: instr._data = 
+            case AluFunc::Slt: instr._data = 
+            case AluFunc::Sltu: instr._data = 
+            case AluFunc::Sll: instr._data = 
+            case AluFunc::Srl: instr._data = 
+            case AluFunc::Sra: instr._data = 
+        }
+        instr._addr = 
+        instr._nextIp
     }
 
 private:
